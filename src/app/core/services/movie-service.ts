50c324cacdc563762099bda306be1f56dabbedb3
Solly Fathi <post2solly@gmail.com>
2026-09-10 20:30:02 +1000

    movies.json updated

--- src/app/core/services/movie-service.ts
+++ src/app/core/services/movie-service.ts
@@ -11,18 +11,6 @@ import { HttpClient } from '@angular/common/http';
 export class MovieService {
   private externalApiService = inject(ExternalApiService)
   private httpClient = inject(HttpClient)
-  private readonly keywords = [
-    'alien',
-    'jedi',
-    'lightsaber',
-    'space',
-    'scifi',
-    'galaxy',
-    'starship',
-    'robot',
-    'spacewar',
-    'futuristic'
-  ];
 
   getPublicResource() {
     return this.httpClient.get<any[]>('assets/movies.json');
@@ -136,12 +124,11 @@ export class MovieService {
 
   generate = (): Observable<ApiResponseModel> => {
     let rnd = Math.random().toString(10).substring(3, 7);
-    let keyword = this.keywords[Math.floor(Math.random() * this.keywords.length)];
     let _body = {
       id: `TEST_${rnd}`,
       title: `Star Wars: The Rise of the Generated Jedi (2025)`,
       year: '2025',
-      poster: `https://loremflickr.com/640/480/${keyword},starwars?lock=123`,
+      poster: `https://picsum.photos/640/480?random=${rnd}`,
       price: 999.99,
       movieRatings: []
     };
